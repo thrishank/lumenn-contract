9915a84038c465a7f7818d9e8dbd9d101495ed7e
thrishank <thrishankkalluru@gmail.com>
2025-08-25 10:34:26 +0530

    add server commands

--- app/app.ts
+++ app/app.ts
@@ -38,6 +38,10 @@ const app = express();
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.get("/fill", async (req, res) => {
   let { order } = req.query;
 
