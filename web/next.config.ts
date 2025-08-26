2c6ba6b98c7acdee99cb7fbde3a08d6e29581088
thrishank <thrishankkalluru@gmail.com>
2025-08-26 15:08:23 +0530

    move web frontend to a seperate repo

--- web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  outputFileTracingRoot:
-    "/Users/thris/Developer/Projects/elara/web/pnpm-lock.yaml",
-  /* config options here */
-};
-
-export default nextConfig;
