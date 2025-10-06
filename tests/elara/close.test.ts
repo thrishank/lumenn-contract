ad6c9d16accbdae088904e03ae1009a37727787b
thrishank <thrishankkalluru@gmail.com>
2025-10-06 15:18:06 +0530

    update close test

--- tests/elara/close.test.ts
+++ tests/elara/close.test.ts
@@ -1,25 +1,50 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Elara } from "../../target/types/elara";
-import { PublicKey, Signer } from "@solana/web3.js";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  Signer,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
 import { TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 describe("elara", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.elara as Program<Elara>;
-  const payer: Signer = program.provider.wallet!.payer!;
+  const keypair: Signer = program.provider.wallet!.payer!;
 
   it("close token 2022 mint", async () => {
-    const tx = await program.methods
+    const instruction = await program.methods
       .closeProtocolAta()
       .accounts({
-        payer: payer.publicKey,
-        mint: new PublicKey("GFScxpTQjxgvsnwVA6DYz1XrKeHebpR5o6FLvZFvkMuw"),
-        tokenProgram: TOKEN_2022_PROGRAM_ID,
+        payer: keypair.publicKey,
+        mint: new PublicKey("JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"),
+        tokenProgram: TOKEN_PROGRAM_ID,
       })
-      .rpc();
+      .instruction();
 
-    console.log(tx);
+    const rpc = new Connection(
+      "https://mainnet.helius-rpc.com/?api-key=c991f045-ba1f-4d71-b872-0ef87e7f039d"
+    );
+
+    const latestBlockhash = await rpc.getLatestBlockhash();
+    const message = new TransactionMessage({
+      payerKey: keypair.publicKey,
+      recentBlockhash: latestBlockhash.blockhash,
+      instructions: [instruction],
+    }).compileToV0Message();
+
+    const tx = new VersionedTransaction(message);
+    tx.sign([keypair]);
+
+    const sim = await rpc.simulateTransaction(tx, { sigVerify: false });
+    console.log(sim);
+
+    const x = await rpc.sendTransaction(tx);
+    console.log(x);
   });
 });
